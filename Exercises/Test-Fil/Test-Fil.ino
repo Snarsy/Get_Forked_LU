267e5653abce83f3c8adc891e51b727eea28bcba
Snarsy <uleberg.lars@gmail.com>
2023-09-14 13:50:17 +0200

    Test for ultrasonisk sensor

--- Exercises/Test-Fil/Test-Fil.ino
+++ Exercises/Test-Fil/Test-Fil.ino
@@ -1,7 +1,22 @@
+const int trigPin = 9;
+const int echoPin = 10;
+long duration;
+int distance;
+
 void setup(){
-    pinMode(13,OUTPUT);
+    pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+    pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+    Serial.begin(9600); // Starts the serial communication
 }
 
 void loop(){
-    digitalWrite(13,HIGH);
-}
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+    distance = duration * 0.034 / 2;
+    Serial.print("Distance: ");
+    Serial.println(distance);
+}   
